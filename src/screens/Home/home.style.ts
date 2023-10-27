7914b49132f5e703dc247f30916c351ba9108b4e
Yahckman <adeniranajibade2014@gmail.com>
2023-10-27 10:35:23 +0100

    banner mobie view

--- src/screens/Home/home.style.ts
+++ src/screens/Home/home.style.ts
@@ -1,20 +1,18 @@
 import styled from 'styled-components'
 
 export const Banner = styled.div.attrs({
-  className: 'font-bold pt-50',
+  className: 'font-bold pt-50 text-center w-full',
 })`
-    background-image: url('../../assets/banner1.png');
-    // font-size:30px;
-    color:red;
-    height:100vh;
-    background-color:#1D1E26;
-    background-position:center;
-    background-size:cover;
-    background-repeat:no-repeat;
-    text-align:center
-   padding: 5px;
-
-    
+  background-image: url('../../assets/banner1.png');
+  // font-size:30px;
+  color: red;
+  // height:100vh;
+  background-color: #1d1e26;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 5px;
+  width: 100%;
 `
 
 export const ParaOne = styled.p.attrs({
@@ -24,19 +22,12 @@ export const ParaOne = styled.p.attrs({
   background: rgba(215, 99, 78, 0.13);
 `
 
-// color: ;
-// text-align: center;
-// font-family: Montserrat;
-// font-size: 48px;
-// font-style: normal;
-// font-weight: 700;
-// line-height: 139.4%; /* 66.912px */
+export const ParaTwo = styled.p.attrs({
+  className:
+    'text-[#CFDDFF] text-center font-bold text-2xl mt-14 md:text-4xl leading-normal mb-8 md:mb-16 ',
+})``
 
-// color: #7E8290;
-
-// text-align: center;
-// font-family: Montserrat;
-// font-size: 18px;
-// font-style: normal;
-// font-weight: 500;
-// line-height: 199.4%;
+export const ParaThree = styled.p.attrs({
+  className:
+    'text-[#7E8290] text-center w-11/12 md:w-6/6 text-lg m-auto leading-7 ',
+})``
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,21 @@
 import CustomButton from '../../components/CustomButton/CustomButton'
-import { Banner, ParaOne } from './home.style'
+import { Banner, ParaOne, ParaThree, ParaTwo } from './home.style'
 
 const Home = () => {
   return (
     <div>
       <Banner>
-        <div className="p-5 w-3/6 flex m-auto flex-col pt-36">
+        <div className="p-10 md:w-3/6 flex m-auto flex-col md:pt-36">
           <ParaOne>135,455 Comunity Members</ParaOne>
 
-          <p className="text-[#CFDDFF] text-center font-bold text-4xl leading-normal mb-16 ">
+          <ParaTwo>
             We connect you with a <br /> community of people like you.
-          </p>
+          </ParaTwo>
 
-          <p className="text-[#7E8290] text-center w-6/6 text-lg m-auto leading-7 ">
+          <ParaThree>
             Join any of our communities and connect with thousands of people who
             share the same interest as you,
-          </p>
+          </ParaThree>
 
           <div className="m-auto mt-9">
             <CustomButton>Start your 3-Day free trial</CustomButton>

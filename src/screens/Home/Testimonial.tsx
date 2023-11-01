6b3013ee976e8c97e7c0e5e75e49abb73ea963ea
Yahckman <adeniranajibade2014@gmail.com>
2023-11-01 11:36:38 +0100

    error fixed

--- src/screens/Home/Testimonial.tsx
+++ src/screens/Home/Testimonial.tsx
@@ -46,7 +46,9 @@ function Testimonial() {
                 alt="imagr"
                 className="mr-3"
               />{' '}
-              <p className="text-[#FFF] text-xs font-semibold mt-0">John Doe</p>
+              <p className="text-[#FFF] text-xs font-semibold mt-0">
+                Frank Major
+              </p>
             </div>
           </div>
         ))}

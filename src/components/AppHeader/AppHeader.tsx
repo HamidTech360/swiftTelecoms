c292b9afb7db24829a26f1b7504ee6442a8f8f17
Yahckman <adeniranajibade2014@gmail.com>
2023-11-03 00:11:31 +0100

    nav

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 import { BiMenuAltRight } from 'react-icons/bi'
+import { useState } from 'react'
 
 function AppHeader() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   const headerData = [
     {
       title: 'Home',
@@ -17,31 +20,64 @@ function AppHeader() {
     },
   ]
   return (
-    <div className="py-7 px-5 lg:px-28 flex justify-between">
-      <div className="text-white">Swift Telecoms logo</div>
-      <div className="text-white hidden lg:block mr-8 ">
-        {headerData.map((item: any, i: number) => (
-          <>
-            <Link
-              to={item.link}
-              key={i}
-              className=" no-underline text-sm font-normal text-[#D2D5DE]"
-            >
-              {item.title}
-            </Link>{' '}
-          </>
-        ))}
-        <Link
-          to="#"
-          className="mr-8 no-underline text-sm rounded-full text-[#FFF] px-4 py-3  bg-[#5B89FF] font-normal ]"
-        >
-          Get started
-        </Link>
-      </div>
+    <div>
+      <div className="py-7 px-5 lg:px-28 flex justify-between">
+        <div className="text-white">Swift Telecoms logo</div>
+        <div className="text-white hidden lg:block mr-8 ">
+          {headerData.map((item: any, i: number) => (
+            <>
+              <Link
+                to={item.link}
+                key={i}
+                className=" no-underline text-sm font-normal text-[#D2D5DE]"
+              >
+                {item.title}
+              </Link>{' '}
+            </>
+          ))}
+          <Link
+            to="#"
+            className="mr-8 no-underline text-sm rounded-full text-[#FFF] px-4 py-3  bg-[#5B89FF] font-normal ]"
+          >
+            Get started
+          </Link>
+        </div>
 
-      <div className=" lg:hidden">
-        <BiMenuAltRight size={30} color={'white'} />
+        <div className=" lg:hidden ">
+          {!isOpen ? (
+            <BiMenuAltRight
+              onClick={() => {
+                setIsOpen(!isOpen)
+              }}
+              size={30}
+              color={'white'}
+            />
+          ) : (
+            //   <BiMenuAltRight onClick={()=>{setIsOpen(!isOpen)}} size={30} color={'white'} />
+            <p
+              onClick={() => {
+                setIsOpen(!isOpen)
+              }}
+            >
+              X
+            </p>
+          )}
+        </div>
       </div>
+      {isOpen && (
+        <div className=" text-center fixed mb-4 top-16  p-2   w-[100%] transition ease-out duration-1000 transform">
+          {headerData.map((item: any, i: any) => (
+            <Link
+              key={i}
+              to={'#'}
+              className="block  mb-5 no-underline text-sm text-[#D2D5DE] transition-opacity duration-500  "
+            >
+              {' '}
+              {item.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }

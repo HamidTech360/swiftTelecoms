c2a6073078be899cdf53593310fde46d54c1eec1
Yahckman <adeniranajibade2014@gmail.com>
2023-11-05 13:55:03 +0100

    adding buttom text to button

--- src/layouts/authLayout/index.tsx
+++ src/layouts/authLayout/index.tsx
@@ -1,7 +1,7 @@
 import { BannerImg, FormArea } from './authlayout.style'
 const AuthLayout = ({ children, title, subTitle }: any) => {
   return (
-    <div className="md:flex h-screen">
+    <div className="md:flex min-h-screen " style={{ height: 'fit-content' }}>
       <div>
         <div className="pt-10 -mb-16 px-5 bg-[#1D1E26] md:hidden">
           <div className="text-white font-bold text-4xl  leading-12">
@@ -19,6 +19,7 @@ const AuthLayout = ({ children, title, subTitle }: any) => {
         </div>
         <div className="flex flex-1 flex-col justify-end lg:pr-20 md:pr-14">
           <BannerImg className="" src="../../assets/bigAvatar.png" alt="" />
+          <BannerImg className="" src="../../assets/bigAvatar.png" alt="" />
         </div>
       </div>
       <div className="md:w-3/5  h-screen  bg-[#1D1E26] py-7 px-5 ">
@@ -29,3 +30,11 @@ const AuthLayout = ({ children, title, subTitle }: any) => {
 }
 
 export default AuthLayout
+
+// color: rgba(255, 255, 255, 0.62);
+
+// font-family: Montserrat;
+// font-size: 13px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 161.9%;
--- src/screens/Auth/Login/index.tsx
+++ src/screens/Auth/Login/index.tsx
@@ -1,32 +1,39 @@
 import AuthLayout from '../../../layouts/authLayout'
 import { Input, Label } from './login.style'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <AuthLayout
-      title="Welcome Back"
-      subTitle="Let's pickup from where you left"
-    >
-      <div className="text-white">
-        <p className="text-center text-[#D7634E] mb-12 font-bold text-base ">
-          Log in
-        </p>
-        <form action="">
-          <div className="mb-6">
-            <Label>Username</Label> <br />
-            <Input type="text" />
-          </div>
-          <div className="mb-6">
-            <Label>Password</Label> <br />
-            <Input type="password" />
-          </div>
+    <div>
+      <AuthLayout
+        title="Welcome Back"
+        subTitle="Let's pickup from where you left"
+      >
+        <div className="text-white">
+          <p className="text-center text-[#D7634E] mb-12 font-bold text-base ">
+            Log in
+          </p>
+          <form action="">
+            <div className="mb-5">
+              <Label>Username</Label> <br />
+              <Input type="text" placeholder="User1234" />
+            </div>
+            <div className="mb-5">
+              <Label>Password</Label> <br />
+              <Input type="password" />
+            </div>
 
-          <button className="w-full pl-3 py-4 rounded-sm border-none m-auto text-[#FFF] text-sm font-semibold  bg-[#5B89FF] cursor-pointer   ">
-            Log In
-          </button>
-        </form>
-      </div>
-    </AuthLayout>
+            <button className="mt-4 w-full pl-3 py-2 md:py-4 rounded-md border-none m-auto text-[#FFF] text-sm font-semibold  bg-[#5B89FF] cursor-pointer   ">
+              Log In
+            </button>
+          </form>
+
+          <p className="text-[#AAABAF] text-sm font-semibold leading-10 text-center">
+            Don’t have an account? <Link to="#">Sign up</Link>{' '}
+          </p>
+        </div>
+      </AuthLayout>
+    </div>
   )
 }
 

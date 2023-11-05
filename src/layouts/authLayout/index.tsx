54d1e6f7260ff65fa0a164d8ad943abc3b274cac
Yahckman <adeniranajibade2014@gmail.com>
2023-11-05 13:58:57 +0100

    remove second bg image

--- src/layouts/authLayout/index.tsx
+++ src/layouts/authLayout/index.tsx
@@ -19,7 +19,6 @@ const AuthLayout = ({ children, title, subTitle }: any) => {
         </div>
         <div className="flex flex-1 flex-col justify-end lg:pr-20 md:pr-14">
           <BannerImg className="" src="../../assets/bigAvatar.png" alt="" />
-          <BannerImg className="" src="../../assets/bigAvatar.png" alt="" />
         </div>
       </div>
       <div className="md:w-3/5  h-screen  bg-[#1D1E26] py-7 px-5 ">

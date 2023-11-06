25eea2b2d8fd4f02416e166b48fc12ba4dfe4d59
Yahckman <adeniranajibade2014@gmail.com>
2023-11-06 11:25:05 +0100

    validate verify form

--- src/screens/Auth/Verify/Verify.tsx
+++ src/screens/Auth/Verify/Verify.tsx
@@ -1,7 +1,29 @@
 import { Link } from 'react-router-dom'
 import AuthLayout from '../../../layouts/authLayout'
+import { useState } from 'react'
 
 function Verify() {
+  const [data, setData] = useState<any>('')
+  const [error, setError] = useState('')
+
+  const handleChange = (e: any) => {
+    const input = e.target.value
+    setData(input)
+
+    if (!/^\d{4}$/.test(input)) {
+      setError('Please enter a 4-digit code.')
+    } else {
+      setError('')
+    }
+  }
+
+  const handleSubmit = () => {
+    if (!error) {
+      console.log(data)
+    } else {
+      console.log('there is an errr')
+    }
+  }
   return (
     <AuthLayout title="Almost Done!">
       <p className="text-[#D7634E] text-center text-2xl font-bold ">
@@ -19,9 +41,14 @@ function Verify() {
           className="border-none outline-none w-full pl-3 py-3 md:py-4 text-[#FFF] placeholder-[#FFFFFF30] text-sm font-normal  rounded-lg p-0 bg-[#2B2D3D] text-center"
           type="text"
           placeholder="_   _   _   _"
+          onChange={handleChange}
         />
+        {error && <div className="text-[#FF0000] text-sm mt-3">{error}</div>}
       </div>
-      <button className="mt-4 w-full pl-3 py-2 md:py-4 rounded-md border-none m-auto text-[#FFF] text-sm font-semibold  bg-[#5B89FF] cursor-pointer   ">
+      <button
+        onClick={handleSubmit}
+        className="mt-4 w-full pl-3 py-2 md:py-4 rounded-md border-none m-auto text-[#FFF] text-sm font-semibold  bg-[#5B89FF] cursor-pointer   "
+      >
         Verify Code
       </button>
 
